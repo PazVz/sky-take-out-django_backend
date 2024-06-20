191b8b66d05ae6786872f12fc16fd074b50992b7
PazVz <PazVzVz@gmail.com>
2024-06-20 13:29:55 +0800

    迁移项目到docker上。

--- django_project/settings.py
+++ django_project/settings.py
@@ -89,11 +89,14 @@ WSGI_APPLICATION = "django_project.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "sky-take-out-django_backend",
+        "USER": "pazv",
+        "PASSWORD": "123456",
+        "HOST": "db",
+        "PORT": "3306",
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
